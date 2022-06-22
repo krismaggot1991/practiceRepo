37910a328d378413bf10db766744f69946c5db2b
Christian Alfredo Muyon Rivera <cmuyonri@pichincha.com>
2022-06-21 19:43:03 -0500

    Implementation getAllClients method and fix settings test

--- src/main/java/com/pichincha/practice/service/impl/ClientServiceImpl.java
+++ src/main/java/com/pichincha/practice/service/impl/ClientServiceImpl.java
@@ -1,15 +1,20 @@
 package com.pichincha.practice.service.impl;
 
 import com.pichincha.practice.domain.jpa.Client;
+import com.pichincha.practice.repository.ClientRepository;
 import com.pichincha.practice.service.ClientService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
+
+  private final ClientRepository clientRepository;
 
   @Override
   public List<Client> getAllClients() {
-    return null;
+    return clientRepository.findAll();
   }
 }
--- src/test/java/com/pichincha/practice/service/ClientServiceTest.java
+++ src/test/java/com/pichincha/practice/service/ClientServiceTest.java
@@ -13,18 +13,20 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith({MockitoExtension.class, SpringExtension.class})
+@WebMvcTest(controllers = ClientServiceImpl.class)
 class ClientServiceTest {
 
-  @InjectMocks
+  @Autowired
   ClientServiceImpl clientService;
 
-  @Mock
+  @MockBean
   ClientRepository clientRepository;
 
   @Test
